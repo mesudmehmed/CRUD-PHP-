1db444b5934aa983368ab0d833f5a231f13583b5
Mesud Mehmed <57757676+mesudmehmed@users.noreply.github.com>
2023-04-27 19:22:36 +0200

    Home pagina met gebruikersnaam herkening

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+// Initialize the session
+session_start();
+include 'header.php';
+include 'menu.php';
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+    <!-- Page content-->
+    <div class="container">
+        <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <!-- Bootstrap core JS-->
+<?php include 'footer.php' ?>

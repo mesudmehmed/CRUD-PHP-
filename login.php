a4df5393cb1968664e4c281219df184b0a7e4e26
Mesud Mehmed <57757676+mesudmehmed@users.noreply.github.com>
2023-04-27 18:24:03 +0200

    Dynamische pagina's

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php include 'header.php' ?>
+    <!-- Responsive navbar-->
+<?php include 'menu.php' ?>
+    <!-- Page content-->
+    <div class="container">
+        <div class="text-center mt-5">
+            <h1>Login</h1>
+            <p class="lead">Login contents</p>
+        </div>
+    </div>
+    <!-- Bootstrap core JS-->
+<?php include 'footer.php' ?>
